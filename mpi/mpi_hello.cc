a0fc3f44af364bf83bf3d527537104ee72937430
Minji Lee <mingg9688@gmail.com>
2021-04-15 22:35:25 +0900

    numerical integration with mpi

--- mpi/mpi_hello.cc
+++ mpi/mpi_hello.cc
@@ -6,7 +6,16 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv)
+{
+    MPI_Init(NULL, NULL);
     cout << "hello" << endl;
+    int rank, size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        cout << "Hello, world! :from " << size << "processes" << endl;
+    }
+    MPI_Finalize();
     return 0;
 }
